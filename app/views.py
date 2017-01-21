ef2f4832f267501e09efbec8b64c8cad9dd13f10
Mike Evans <ihackstuff85@gmail.com>
2017-01-21 03:20:58 +0000

    added a few more methods

--- app/views.py
+++ app/views.py
@@ -3,15 +3,17 @@ from app import BBFrameworkAPP, BBFrameworkAPI, BBFrameworkDB, Resource, reqpars
 ## BBFrameworkAPP Classes
 ## Engagements:
 class Engagements(Resource):
+	## Parse any parameters we require:
 	def __init__(self):
 		self.reqparse = reqparse.RequestParser()
 		self.reqparse.add_argument('eng_name', type=str, required=True, help="eng_name missing", location='json')
 		self.reqparse.add_argument('eng_desc', type=str, required=True, help="eng_desc missing", location='json')
 		self.reqparse.add_argument('eng_user_id', type=str, required=True, help="eng_user_id missing", location='json')
 		super(Engagements, self).__init__()
-
+	## Required to check if a new database entry is unique or not
+	## to avoid duplicates within the database and the id's going out of sync.
 	def is_unique(self, new_eng):
-		is_unique = False
+		is_unique = True
 		current_engs = models.Engagements.query.all()
 		for e in current_engs:
 			if (new_eng.eng_name in e.eng_name) or (new_eng.eng_desc in e.eng_desc):
@@ -21,22 +23,24 @@ class Engagements(Resource):
 				is_unique = True
 		return is_unique
 
+	## Get the complete list of enagements:
 	def get(self):
 		engs = models.Engagements.query.all()
 		return { 'Engagements: ' : marshal(engs, datastructures.engagement_fields) }
 
+	## Create an engagement:
 	def post(self):
 		try:
 	                args = self.reqparse.parse_args()
 		except:
-			return {"Error: " : "Invalid JSON data sent."}
+			return {"Error: " : "Invalid JSON data sent"}
 		
 		e_name = args['eng_name']
 		e_desc = args['eng_desc']
 		e_u_id = args['eng_user_id']
 		
 		if not e_name or not e_desc or not e_u_id:
-                        return {"Error: " : "One of (e_name, e_desc or e_u_id values is missing"}
+                        return {"Error: " : "Either of e_name, e_desc or e_u_id values is missing"}
 		
 		new_engagement = models.Engagements(eng_name = args['eng_name'], eng_desc = args['eng_desc'], eng_user_id = args['eng_user_id'])
 
@@ -56,8 +60,10 @@ class Engagements(Resource):
 		else:
 			return {"Error: " : "The new engagement is a duplicate, duplicates cannot exist!"}
 
+
 ## Specific Engagement:
 class Specific_Engagement(Resource):
+        ## Parse any parameters we require:
 	def __init__(self):
 		self.reqparse = reqparse.RequestParser()
 		self.reqparse.add_argument('eng_name', type=str, required=True, help="eng_name missing", location='json')
@@ -65,11 +71,13 @@ class Specific_Engagement(Resource):
                 self.reqparse.add_argument('eng_user_id', type=str, required=True, help="eng_user_id missing", location='json')
                 super(Specific_Engagement, self).__init__()
 
+	## Request a specific engagement:
 	def get(self, eng_id):
 		s_eng = models.Engagements.query.get(eng_id)
 		eng_string = "Engagement: %s" % eng_id
 		return {eng_string : marshal(s_eng, datastructures.engagement_fields)}
 
+	## Update engagement details:
 	def put(self, eng_id):	
 		## Check JSON data sent by client is correct.
 		try:
@@ -92,6 +100,7 @@ class Specific_Engagement(Resource):
 		BBFrameworkDB.session.commit()
 		return {"Updated Engagement: " : marshal(tmp_engagement, datastructures.engagement_fields)}
 
+	## Delete an engagement:
 	def delete(self, eng_id):	
 		if (models.Engagements.query.filter_by(eng_id = eng_id).delete()):
 			BBFrameworkDB.session.commit()
@@ -117,18 +126,91 @@ class Specific_Module(Resource):
 
 ## Users:
 class Users(Resource):
+        def __init__(self):
+	        self.reqparse = reqparse.RequestParser()
+                self.reqparse.add_argument('username', type=str, required=True, help="username missing", location='json')
+                self.reqparse.add_argument('password', type=str, required=True, help="password missing", location='json')
+                super(Users, self).__init__()
+
+	## Implement is_unique for users:
+	def is_unique(self):
+		pass
+	
+	## Get a complete list of users:
         def get(self):
-                return "Lists all users<BR />"
-        def post(self, id):
-                return "Creates a new user<BR />"
+		users = models.Users.query.all()
+                return {"Users: " : marshal(users, datastructures.user_fields)}
+
+	## Create a user:
+        def post(self):
+		try:	
+			args = self.reqparse.parse_args()
+		except:	
+			return {"Error: " : "Invalid JSON data sent"}
+			
+		username = args['username']
+		password = args['password']
+		
+		if not username or not password:
+			return {"Error: " : "One of the username or password values is missing"}
+
+		new_user = models.Users(username = username, password = password)
+
+		try:
+	                BBFrameworkDB.session.add(new_user)
+                        BBFrameworkDB.session.commit()
+                        return {'User Added: ' : marshal(new_user, datastructures.user_fields)}
+                except exc.IntegrityError as e:
+                        BBFrameworkDB.session.rollback()
+                        err = {'err_type' : type(e), 'err_desc' : e.args}
+                        return {"Error: " : marshal(err, datastructures.error_fields)}
+                except exc.OperationalError as e:
+                        BBFrameworkDB.session.rollback()
+                        err = {'err_type' : type(e), 'err_desc' : e.args}
+                        return {"Error: " : marshal(err, datastructures.error_fields)}
+
 
 class Specific_User(Resource):
+	def __init__(self):
+                self.reqparse = reqparse.RequestParser()
+                self.reqparse.add_argument('username', type=str, required=True, help="username missing", location='json')
+                self.reqparse.add_argument('password', type=str, required=True, help="password missing", location='json')
+                super(Specific_User, self).__init__()
+
+	## List a specifc user:	
 	def get(self, user_id):
-		return "User details: %s" % user_id
+		user = models.Users.query.get(user_id)
+		return {"User: " : marshal(user, datastructures.user_fields)}
+
+	## Update user record:
 	def put(self, user_id):
-		return "Update specific users details: %s" % user_id
+		#try:
+		args = self.reqparse.parse_args()
+		#except:
+		#	return {"Error: " : "Invalid JSON data sent."}
+		print "debug: %r" % args
+		## check the values are not empty:
+		username = args['username']
+		password = args['password']
+
+		if not username or not password or not user_id:
+			return {"Error: " : "Either the username, password or user_id value is missing."}	
+
+		## Update user:
+		tmp_user = models.Users.query.get(user_id)
+		tmp_user.username = username
+		tmp_user.password = password
+		tmp_user.user_id  = user_id
+		BBFrameworkDB.session.commit()
+		return {"Updated User: " : marshal(tmp_user, datastructures.user_fields)}
+
+	## Delete a specific user:
 	def delete(self, user_id):
-		return "Delete a specific user: %s" % user_id
+		if (models.Users.query.filter_by(user_id = user_id).delete()):
+			BBFrameworkDB.session.commit()
+			return {"Deleted: " : True }
+		else:	
+			return {"Deleted: " : False }
 
 	
 
